2c38185bfba6220bba38f2265ef490ca95b25f85
Richie-Z <zakariarichie@yahoo.com>
2023-11-06 22:17:55 +0700

    feat(common-component): introduce Header component

---
+++ src/common/components/Header/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './styles.scss'
+
+type HeaderProps = {
+  includeHero: boolean
+}
+
+const Header: React.FC<HeaderProps> = ({ includeHero = false }) => {
+  return (
+    <section id="hero">
+      <nav className="header-nav">
+        <div className="logo-bx">
+          <svg width="100" height="34" viewBox="0 0 100 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="100" height="34" fill="#0D28A6" />
+          </svg>
+        </div>
+        <div className="quick-link">
+          <a href="">Our Service</a>
+          <a href="">Why Us</a>
+          <a href="">Testimonial</a>
+          <a href="">FAQ</a>
+        </div>
+      </nav>
+      {includeHero && (
+        <div className="hero-section">
+          <div className="left">
+            <h1>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
+            <p>
+              Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas
+              terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu
+              untuk sewa mobil selama 24 jam.
+            </p>
+            <button className="btn">Mulai Sewa Mobil</button>
+          </div>
+          <div className="right">
+            <img src="/images/mercedes-car.png" alt="Mercedes Car" />
+            <svg width="704" height="243" viewBox="0 0 704 243" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 60C0 26.8629 26.8629 0 60 0H704V243H0V60Z" fill="#0D28A6" />
+            </svg>
+          </div>
+        </div>
+      )}
+    </section>
+  )
+}
+
+export default Header;
