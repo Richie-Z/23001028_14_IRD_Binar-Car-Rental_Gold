646f6c0be0ff14703c0c2440ad50b6b4b956212c
Richie-Z <zakariarichie@yahoo.com>
2023-11-06 22:59:50 +0700

    feat(components): introduce footer

---
+++ src/common/components/Footer/index.tsx
@@ -0,0 +1,61 @@
+import './styles.scss'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="information">
+        <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
+        <p>binarcarrental@gmail.com</p>
+        <p>081-233-334-808</p>
+      </div>
+      <div className="link">
+        <a href="">Our services</a>
+        <a href="">Why Us</a>
+        <a href="">Testimonial</a>
+        <a href="">FAQ</a>
+      </div>
+      <div className="connect">
+        <p>Connect with us</p>
+        <div className="icons">
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="16" cy="16" r="16" />
+            <path
+              d="M21 7.66663H18.5C17.395 7.66663 16.3352 8.10561 15.5538 8.88701C14.7724 9.66842 14.3334 10.7282 14.3334 11.8333V14.3333H11.8334V17.6666H14.3334V24.3333H17.6667V17.6666H20.1667L21 14.3333H17.6667V11.8333C17.6667 11.6123 17.7545 11.4003 17.9108 11.244C18.0671 11.0878 18.279 11 18.5 11H21V7.66663Z"
+              stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="16" cy="16" r="16" />
+            <path
+              d="M21 7.66663H18.5C17.395 7.66663 16.3352 8.10561 15.5538 8.88701C14.7724 9.66842 14.3334 10.7282 14.3334 11.8333V14.3333H11.8334V17.6666H14.3334V24.3333H17.6667V17.6666H20.1667L21 14.3333H17.6667V11.8333C17.6667 11.6123 17.7545 11.4003 17.9108 11.244C18.0671 11.0878 18.279 11 18.5 11H21V7.66663Z"
+              stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="16" cy="16" r="16" />
+            <path
+              d="M21 7.66663H18.5C17.395 7.66663 16.3352 8.10561 15.5538 8.88701C14.7724 9.66842 14.3334 10.7282 14.3334 11.8333V14.3333H11.8334V17.6666H14.3334V24.3333H17.6667V17.6666H20.1667L21 14.3333H17.6667V11.8333C17.6667 11.6123 17.7545 11.4003 17.9108 11.244C18.0671 11.0878 18.279 11 18.5 11H21V7.66663Z"
+              stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="16" cy="16" r="16" />
+            <path
+              d="M21 7.66663H18.5C17.395 7.66663 16.3352 8.10561 15.5538 8.88701C14.7724 9.66842 14.3334 10.7282 14.3334 11.8333V14.3333H11.8334V17.6666H14.3334V24.3333H17.6667V17.6666H20.1667L21 14.3333H17.6667V11.8333C17.6667 11.6123 17.7545 11.4003 17.9108 11.244C18.0671 11.0878 18.279 11 18.5 11H21V7.66663Z"
+              stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="16" cy="16" r="16" />
+            <path
+              d="M21 7.66663H18.5C17.395 7.66663 16.3352 8.10561 15.5538 8.88701C14.7724 9.66842 14.3334 10.7282 14.3334 11.8333V14.3333H11.8334V17.6666H14.3334V24.3333H17.6667V17.6666H20.1667L21 14.3333H17.6667V11.8333C17.6667 11.6123 17.7545 11.4003 17.9108 11.244C18.0671 11.0878 18.279 11 18.5 11H21V7.66663Z"
+              stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </div>
+      </div>
+      <div className="copyright">
+        <p>Copyright Binar 2022</p>
+        <svg width="100" height="34" viewBox="0 0 100 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="100" height="34" />
+        </svg>
+      </div>
+    </footer>
+  )
+}
+
--- src/modules/landing/pages/Landing.tsx
+++ src/modules/landing/pages/Landing.tsx
@@ -1,9 +1,11 @@
 import Header from "@/common/components/Header"
+import Footer from "@/common/components/Footer"
 
 export default function Landing() {
   return (
     <>
       <Header includeHero={true} />
+      <Footer />
     </>
   )
 }
