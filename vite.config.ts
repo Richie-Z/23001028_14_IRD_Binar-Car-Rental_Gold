37c376d26238bb6de8a6efb4e435f3bb3fd860b0
Richie-Z <zakariarichie@yahoo.com>
2023-11-06 20:06:00 +0700

    enhance(vite): setup the alias; remove the boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    }
+  },
+  server: {
+    port: 3000
+  }
 })
