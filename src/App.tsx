881feefd6e355c7df83b3f3871427d5ed03e5f9b
Richie-Z <zakariarichie@yahoo.com>
2023-11-08 00:32:17 +0700

    fix(app): the element is not react so use React.createElement

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Landing from "@/modules/landing/pages/Landing"
 import Search from "@/modules/search/pages/Search"
-
+import React from 'react'
 
 const routes = [
-  { path: '/', element: Landing() },
-  { path: '/search', element: Search() },
+  { path: '/', element: Landing },
+  { path: '/search', element: Search },
 ]
 
 export default function App() {
@@ -14,7 +14,7 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           {routes.map((val, i) => (
-            <Route key={i} path={val.path} element={val.element} />
+            <Route key={i} path={val.path} element={React.createElement(val.element)} />
           ))}
         </Routes>
       </BrowserRouter>
