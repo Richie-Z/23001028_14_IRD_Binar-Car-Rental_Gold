909396741e75594291b93e8ba4a8ba4c8d512d86
Richie-Z <zakariarichie@yahoo.com>
2023-11-07 23:32:47 +0700

    enhance(cta): usage of to props

--- src/modules/landing/components/CTA/index.tsx
+++ src/modules/landing/components/CTA/index.tsx
@@ -8,7 +8,7 @@ export default function CTA() {
         <h1>Sewa Mobil di (Lokasimu) Sekarang</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. </p>
-        <Button title="Mulai Sewa Mobil" />
+        <Button title="Mulai Sewa Mobil" to="search" />
       </div>
     </section>
   )
