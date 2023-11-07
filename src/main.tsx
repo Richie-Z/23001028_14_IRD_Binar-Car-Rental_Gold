6160a81b8ccf2924c0afb1a14676bec00a2ee0eb
Richie-Z <zakariarichie@yahoo.com>
2023-11-08 00:59:05 +0700

    refactor: style naming file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.tsx'
-import '@/common/styles/global.scss'
+import '@/common/styles/_global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
