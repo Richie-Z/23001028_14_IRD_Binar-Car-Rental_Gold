efe1524052cdfb38b4ada00c2e4e7433d7677c99
Richie-Z <zakariarichie@yahoo.com>
2023-11-08 00:51:39 +0700

    refactor: move routes to new file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Landing from "@/modules/landing/pages/Landing"
-import Search from "@/modules/search/pages/Search"
 import React from 'react'
-
-const routes = [
-  { path: '/', element: Landing },
-  { path: '/search', element: Search },
-]
+import routes from './routes'
 
 export default function App() {
   return (
---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import Landing from "@/modules/landing/pages/Landing"
+import Search from "@/modules/search/pages/Search"
+
+const routes = [
+  { path: '/', element: Landing },
+  { path: '/search', element: Search },
+]
+
+export default routes
