b2426bda6a6cf33fa96e86895741e3a689ccc53c
Richie-Z <zakariarichie@yahoo.com>
2023-11-09 15:57:23 +0700

    enhance(car): usage of formatRupiah utils on carcard component

--- src/features/car/components/CarCard/index.tsx
+++ src/features/car/components/CarCard/index.tsx
@@ -3,12 +3,9 @@ import './styles.scss'
 import Button from '@/common/components/Button';
 import { CarType } from '../..';
 import React from 'react';
+import formatRupiah from '@/common/utils/formatRupiah';
 
-type CarCardProps = {
-  car: CarType
-}
-
-export const CarCard: React.FC<CarCardProps> = ({ car }) => {
+export const CarCard: React.FC<{ car: CarType }> = ({ car }) => {
   const navigate = useNavigate()
   return (
     <div className="item-container">
@@ -16,7 +13,7 @@ export const CarCard: React.FC<CarCardProps> = ({ car }) => {
         <img src={car.image} alt="" />
       </div>
       <p className="item-title">{car.name}</p>
-      <p className="item-price">{`Rp. ${car.price} / hari`}</p>
+      <p className="item-price">{formatRupiah(car.price)}</p>
       <p className="item-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
         labore et dolore magna aliqua. </p>
       <Button title="Pilih Mobil" onClick={() => navigate(`/detail/${car.id}`)} />
