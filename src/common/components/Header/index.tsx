3dec40abc80cd4b19043088b5543632ca3a1fdad
Richie-Z <zakariarichie@yahoo.com>
2023-11-10 16:39:16 +0700

    enhance(header): mobile view

--- src/common/components/Header/index.tsx
+++ src/common/components/Header/index.tsx
@@ -25,6 +25,13 @@ const Header: React.FC<HeaderProps> = ({ includeHero = false, includeBtn = true 
           <a href="#testimonial">Testimonial</a>
           <a href="#faq">FAQ</a>
         </div>
+        <div className="hamburger" onClick={() => alert('hello mom')}>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 18H21" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M3 12H21" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M3 6H21" stroke="#222222" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </div>
       </nav>
       {includeHero && (
         <div className="hero-section">
@@ -41,7 +48,7 @@ const Header: React.FC<HeaderProps> = ({ includeHero = false, includeBtn = true 
           </div>
           <div className="right">
             <img src="/images/mercedes-car.png" alt="Mercedes Car" />
-            <svg width="704" height="243" viewBox="0 0 704 243" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg viewBox="0 0 704 243" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M0 60C0 26.8629 26.8629 0 60 0H704V243H0V60Z" />
             </svg>
           </div>
